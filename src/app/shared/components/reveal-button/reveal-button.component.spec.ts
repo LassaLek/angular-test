065f641b610a7d4082f7cf643d6d636e38290a14
petr hoza <petr.hoza@ceosdata.com>
2020-10-11 09:06:44 +0200

    add button component

---
+++ src/app/shared/components/reveal-button/reveal-button.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { RevealButtonComponent } from './reveal-button.component';
+
+describe('RevealButtonComponent', () => {
+  let component: RevealButtonComponent;
+  let fixture: ComponentFixture<RevealButtonComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ RevealButtonComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(RevealButtonComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/shared/components/reveal-button/reveal-button.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-reveal-button',
+  templateUrl: './reveal-button.component.html',
+  styleUrls: ['./reveal-button.component.scss']
+})
+export class RevealButtonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
