ef5a4ab6bb3847f997b17e412855298fa76c360d
michal <michal@kajnak.net>
2020-10-17 11:33:16 +0200

    revealing button directive

--- src/app/shared/directives/hover-effect.directive.ts
+++ src/app/shared/directives/hover-effect.directive.ts
@@ -3,24 +3,25 @@ import { Directive, ElementRef, HostListener } from '@angular/core';
 @Directive({
   selector: '[appHoverEffect]'
 })
+
 export class HoverEffectDirective {
   private el: HTMLElement;
 
   @HostListener('mouseenter')
   onMouseEnter() {
-    //TODO změnit stylování elementu, možno přímo úpravou stylů či například měněním classy
-    this.el.style.backgroundColor = '#a83246';
-    this.el.style.color = 'black'
+    let editorButton = this.el.querySelector('.btn-je');
+    editorButton.classList.add("hover");    
   }
 
   @HostListener('mouseleave')
   onMouseLeave() {
-    //TODO změnit stylování elementu, možno přímo úpravou stylů či například měněním classy
-    this.el.style.backgroundColor = '#1976d2';
-    this.el.style.color = 'white'
+    let editorButton = this.el.querySelector('.btn-je');
+    editorButton.classList.remove("hover");
   }
 
-
-  constructor(el: ElementRef) { this.el = el.nativeElement; }
+  constructor(el: ElementRef) { 
+    this.el = el.nativeElement; 
+  }
 
 }
+
